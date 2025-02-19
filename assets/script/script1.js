59d63678abb1ca0e78d471f76b1a246484ee4f03
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-02-19 00:43:06 -0500

    animtions

--- assets/script/script1.js
+++ assets/script/script1.js
@@ -33,25 +33,33 @@ const nav = document.getElementById('nav');
 const navHeight = nav.getBoundingClientRect().height;
 
 projects.forEach(project => {
-  project.addEventListener('click', function (el) {``
+  project.addEventListener('click', function() {``
     // Close all the other projects
+    const infoContainer = this.querySelector('.info-container');
+    const isShowing = Array.from(infoContainer.classList).includes('show')
+    
+    if(isShowing){ 
+      infoContainer.classList.toggle('fade-out')
+      setTimeout(()=>{
+        infoContainer.classList.toggle('show');
+      },200)
+    } else {
+      infoContainer.classList.toggle('show');
+      // Scroll to the top of the project
+        window.scrollTo({
+          top: this.getBoundingClientRect().top + window.scrollY - navHeight,
+          behavior: 'smooth',
+        });
+    }
+
     projects.forEach(otherProject => {
-      if (otherProject !== this) {
-        otherProject.querySelector('.project-info')?.classList.remove('show');
-        otherProject.querySelector('.info-container')?.classList.remove('show');
+      if(otherProject !== this){ 
+        setTimeout(()=>{
+          otherProject.querySelector('.info-container')?.classList.remove('show');
+          infoContainer.classList.remove('fade-out')
+        },200)
       }
     });
-
-    // Show the content inside the clicked project
-    const projectInfo = this.querySelector('.project-info');
-    const infoContainer = this.querySelector('.info-container');
-    projectInfo.classList.toggle('show');
-    infoContainer.classList.toggle('show');
     
-    // Scroll to the top of the project with 100px of breathing room
-    window.scrollTo({
-      top: this.getBoundingClientRect().top + window.scrollY - navHeight,
-      behavior: 'smooth',
-    });
   });
 });

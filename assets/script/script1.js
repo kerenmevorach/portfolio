dad5f0e94930aa15e19d4862901d9e768705a63d
kerenmevorach <mevok873@newschool.edu>
2025-02-19 00:39:26 -0500

    scroll behavior, video issues

--- assets/script/script1.js
+++ assets/script/script1.js
@@ -1,13 +1,13 @@
-const body = document.querySelector('#back-to-top');
-const toTop = document.querySelectorAll('.to-top');
+const body = document.querySelector("#back-to-top");
+const toTop = document.querySelectorAll(".to-top");
 
-const circleAnimation = document.querySelector('.circle-animation');
-const section = document.querySelector('section');
+const circleAnimation = document.querySelector(".circle-animation");
+const section = document.querySelector("section");
 
-toTop.forEach(button => {
-  button.addEventListener('click', event => {
+toTop.forEach((button) => {
+  button.addEventListener("click", (event) => {
     event.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   });
 });
 
@@ -27,31 +27,32 @@ toTop.forEach(button => {
 //   scrollToggle(x) // Call listener function at run time
 //   x.addListener(scrollToggle) // Attach listener function on state changes
 
-const projects = Array.from(document.getElementsByClassName('project'));
+const projects = Array.from(document.getElementsByClassName("project"));
 
-const nav = document.getElementById('nav');
+const nav = document.getElementById("nav");
 const navHeight = nav.getBoundingClientRect().height;
 
-projects.forEach(project => {
-  project.addEventListener('click', function (el) {``
+projects.forEach((project) => {
+  project.addEventListener("click", function (el) {
+    ``;
     // Close all the other projects
-    projects.forEach(otherProject => {
+    projects.forEach((otherProject) => {
       if (otherProject !== this) {
-        otherProject.querySelector('.project-info')?.classList.remove('show');
-        otherProject.querySelector('.info-container')?.classList.remove('show');
+        otherProject
+          .querySelector(".info-container")
+          ?.classList.add("fade-out");
+        otherProject.querySelector(".info-container")?.classList.remove("show");
       }
     });
 
     // Show the content inside the clicked project
-    const projectInfo = this.querySelector('.project-info');
-    const infoContainer = this.querySelector('.info-container');
-    projectInfo.classList.toggle('show');
-    infoContainer.classList.toggle('show');
-    
+    const infoContainer = this.querySelector(".info-container");
+    infoContainer.classList.toggle("show");
+
     // Scroll to the top of the project with 100px of breathing room
     window.scrollTo({
       top: this.getBoundingClientRect().top + window.scrollY - navHeight,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   });
 });
